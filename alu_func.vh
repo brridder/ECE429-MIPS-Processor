e57d8937662bec6da1688752d80086db248494d5
Stephan van den Heuvel <stephanvandenheuvel@gmail.com>
2012-07-03 20:07:48 -0400

    added all supported r-type instructions and tests for them

---
+++ alu_func.vh
@@ -0,0 +1,22 @@
+//
+// ALU function definitions
+//
+
+`ifndef _alu_func_vh_
+`define _alu_func_vh_
+
+`define ADD  6'b100000
+`define ADDU 6'b100001
+`define SUB  6'b100010
+`define SUBU 6'b100011
+`define SLT  6'b101010
+`define SLTU 6'b101011
+`define SLL  6'b000000
+`define SRL  6'b000010
+`define SRA  6'b000011
+`define AND  6'b100100
+`define OR   6'b100101
+`define XOR  6'b100110
+`define NOR  6'b100111
+
+`endif
--- alu_tb.v
+++ alu_tb.v
@@ -5,6 +5,7 @@
 //
 
 `include "control.vh"
+`include "alu_func.vh"
 
 module alu_tb;
     reg clock;
@@ -12,13 +13,18 @@ module alu_tb;
     reg[0:31] rtData;
     reg[0:`CONTROL_REG_SIZE-1] control;
     reg[0:5] funct;
+    reg[0:5] sa;
+    wire[0:31] aluOutput;
+    
 
     alu DUT(
         .clock (clock),
 	.rsData (rsData),
         .rtData (rtData),
 	.control (control),
-	.funct (funct)
+	.funct (funct),
+	.outData (aluOutput),
+	.sa (sa)
     );
 
     initial begin
@@ -32,7 +38,7 @@ module alu_tb;
     event terminate_sim;
     initial begin
 	@ (terminate_sim);
-	#10 finish;
+	#10 $finish;
     end
 
     initial begin
@@ -41,7 +47,108 @@ module alu_tb;
 	rsData = 32'h00000005;
 	rtData = 32'h00000002;
 	//arithmetic function set to ADD
-	funct = 6'b100000;
+	funct = `ADD;
+	@ (posedge clock);
+	@ (posedge clock);
+	$display("ALU output for 5 + 2: %d", aluOutput);
+
+	rsData = -32'h00000002;
+	rtData = 32'h00000002;
+	//arithmetic function set to ADD
+	funct = `ADD;
+	@ (posedge clock);
+	@ (posedge clock);
+	$display("ALU output for %d + %d : %d",$signed(rsData), rtData, aluOutput);
+
+	rsData = -32'h00000002;
+	rtData = 32'h00000001;
+	//arithmetic function set to ADDU
+	funct = `ADDU;
+	@ (posedge clock);
+	@ (posedge clock);
+	$display("ALU output for %d + %d (unsigned): %d",rsData, rtData, aluOutput);
+
+	rsData = 32'h00000004;
+	rtData = 32'h00000002;
+	funct = `SUB;
+	@ (posedge clock);
+	@ (posedge clock);
+	$display("ALU output for %d - %d : %d",rsData, rtData, aluOutput);
+
+	rsData = -32'h00000002;
+	rtData = 32'h00000007;
+	funct = `SUBU;
+	@ (posedge clock);
+	@ (posedge clock);
+	$display("ALU output for %d - %d (unsigned): %d",rsData, rtData, aluOutput);
+
+	rsData = -32'h00000002;
+	rtData = 32'h00000007;
+	funct = `SLT;
+	@ (posedge clock);
+	@ (posedge clock);
+	$display("ALU output for %d < %d : %d", $signed(rsData), rtData, aluOutput);
+
+	rsData = -32'h00000002;
+	rtData = 32'h00000007;
+	funct = `SLTU;
+	@ (posedge clock);
+	@ (posedge clock);
+	$display("ALU output for %d < %d (unsigned): %d",rsData, rtData, aluOutput);
+
+	rsData = 32'h00000002;
+	rtData = 32'h00000002;
+	sa = 5'b00010;
+	funct = `SLL;
+	@ (posedge clock);
+	@ (posedge clock);
+	$display("ALU output for %d << %d: %d", rtData, sa, aluOutput);
+
+	rsData = 32'h00000002;
+	rtData = 32'h00000002;
+	sa = 5'b00010;
+	funct = `SRL;
+	@ (posedge clock);
+	@ (posedge clock);
+	$display("ALU output for %d >> %d: %d", rtData, sa, aluOutput);
+
+	rsData = 32'h00000002;
+	rtData = 32'h00000001;
+	sa = 5'b00010;
+	funct = `SRA;
+	@ (posedge clock);
+	@ (posedge clock);
+	$display("ALU output for %d >>> %d: %d", rtData, sa, aluOutput);
+
+	rsData = 32'h00040a02;
+	rtData = 32'hffffffff;
+	funct = `AND;
+	@ (posedge clock);
+	@ (posedge clock);
+	$display("ALU output for %h AND %h: %h", rsData, rtData, aluOutput);
+
+	rsData = 32'h00040a02;
+	rtData = 32'hffff0000;
+	funct = `OR;
+	@ (posedge clock);
+	@ (posedge clock);
+	$display("ALU output for %h OR %h: %h", rsData, rtData, aluOutput);
+
+	rsData = 32'h00040a02;
+	rtData = 32'hffffffff;
+	funct = `XOR;
+	@ (posedge clock);
+	@ (posedge clock);
+	$display("ALU output for %h XOR %h: %h", rsData, rtData, aluOutput);
+
+	rsData = 32'h00040a02;
+	rtData = 32'hffffffff;
+	funct = `NOR;
+	@ (posedge clock);
+	@ (posedge clock);
+	$display("ALU output for %h NOR %h: %h", rsData, rtData, aluOutput);
+
+	-> terminate_sim;
     end
 
 	
