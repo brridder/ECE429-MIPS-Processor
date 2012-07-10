75469a24d12d4e80f7d5f29912df183a614911b0
ravil-b <ravil.almaty@gmail.com>
2012-07-10 17:02:02 -0400

    Fixed indentation in alu.v

--- alu.v
+++ alu.v
@@ -52,124 +52,120 @@ module alu (
     assign insn_index = insn[6:31];
 
     always @(posedge clock)
-      begin
-	  bt = 0;
-          $display("ALU insn %X", insn);          
-	if (control[`R_TYPE]) begin
-	case(funct)
-	`ADD:
-	  //rd <- rs + rt
-	  outData = rsData + rtData;
-	`ADDU:
-	  //todo: do we need to treat unsigned specially?
-	  outData = $unsigned(rsData) + $unsigned(rtData);
-	`SUB:
-	  outData = rsData - rtData;
-	`SUBU:
-	  //todo: correct way to do unsigned?
-	  outData = $unsigned(rsData) - $unsigned(rtData);
-	`SLT: // rd <- 1 if rs < rt; else rd <- 0
-	  if ($signed(rsData) < $signed(rtData)) begin
-	      outData = 32'h00000001;
-	  end else begin
-	      outData = 32'h00000000;
-	  end
-	`SLTU:
-	   if ($unsigned(rsData) < $unsigned(rtData)) begin
-	       outData = 32'h0000001;
-	   end else begin 
-	       outData = 32'h00000000; 
-	   end
-	`SLL:
-	   outData = rtData << sa;
-	`SRL:
-	   outData = rtData >> sa;
-	`SRA:
-	   outData = rtData >>> sa;
-	`AND:
-	   outData = rsData & rtData;
-	`OR:
-	  outData = rsData | rtData;
-	`XOR:
-	  outData = rsData ^ rtData;
-	`NOR:
-	  outData = ~(rsData | rtData);
-	endcase // case (funct)
-	end else if(control[`I_TYPE]) begin
-	    case(opcode)
-	      `ADDIU:
-		outData = rsData + $signed(immediate);
-	      `SLTI:
-		if ($signed(rsData) < $signed(immediate)) begin
-		     outData = 32'h0000001;
-		end else begin
-		     outData = 32'h0000000;
-		end
-	      `LW:
-		outData = $signed(offset) + rsData;
-	      `SW:
-		outData = $signed(offset) + rsData;
-	      `LUI:
-		outData = immediate << 16;
-	      `ORI:
-		outData = rsData | immediate;
-	    endcase // case (funct)
-	end else if (control[`J_TYPE]) begin
-        case(opcode)
-	      `J:
-		//outData = insn_index;
-	        bt = 1'b1;
-		 
-	      `BEQ:
-		if (rsData == rtData) begin
-		    outData = pc + $signed(offset << 2);
-		    bt = 1'b1;
-		end else begin
-		    bt = 1'b0;
-		end
-	      `BNE:
-		if  (rsData  != rtData) begin
-		    outData = pc + $signed(offset << 2);
-		    bt = 1'b1;
-		end else begin
-		    bt = 1'b0;
-		end
-	      `BGTZ:
-		if ($signed(rsData) > 1'b0) begin
-		    outData = pc + $signed(offset << 2);
-		    bt = 1'b1;
-		end else begin
-		    bt = 1'b0;
-		end
-	      `BLEZ:
-		if ($signed(rsData) <= 1'b0) begin
-		    outData = pc + $signed(offset << 2);
-		end else begin
-		    bt = 1'b0;
-		end
+    begin
+	    bt = 0;          
+	    if (control[`R_TYPE]) begin
+	        case(funct)
+	            `ADD:
+	              //rd <- rs + rt
+	              outData = rsData + rtData;
+	            `ADDU:
+	              //todo: do we need to treat unsigned specially?
+	              outData = $unsigned(rsData) + $unsigned(rtData);
+	            `SUB:
+	              outData = rsData - rtData;
+	            `SUBU:
+	              //todo: correct way to do unsigned?
+	              outData = $unsigned(rsData) - $unsigned(rtData);
+	            `SLT: // rd <- 1 if rs < rt; else rd <- 0
+	              if ($signed(rsData) < $signed(rtData)) begin
+	                  outData = 32'h00000001;
+	              end else begin
+	                  outData = 32'h00000000;
+	              end
+	            `SLTU:
+	              if ($unsigned(rsData) < $unsigned(rtData)) begin
+	                  outData = 32'h0000001;
+	              end else begin 
+	                  outData = 32'h00000000; 
+	              end
+	            `SLL:
+	              outData = rtData << sa;
+	            `SRL:
+	              outData = rtData >> sa;
+	            `SRA:
+	              outData = rtData >>> sa;
+	            `AND:
+	              outData = rsData & rtData;
+	            `OR:
+	              outData = rsData | rtData;
+	            `XOR:
+	              outData = rsData ^ rtData;
+	            `NOR:
+	              outData = ~(rsData | rtData);
+	        endcase // case (funct)
+	    end else if(control[`I_TYPE]) begin
+	        case(opcode)
+	            `ADDIU:
+		          outData = rsData + $signed(immediate);
+	            `SLTI:
+		          if ($signed(rsData) < $signed(immediate)) begin
+		              outData = 32'h0000001;
+		          end else begin
+		              outData = 32'h0000000;
+		          end
+	            `LW:
+		          outData = $signed(offset) + rsData;
+	            `SW:
+		          outData = $signed(offset) + rsData;
+	            `LUI:
+		          outData = immediate << 16;
+	            `ORI:
+		          outData = rsData | immediate;
+	        endcase // case (funct)
+	    end else if (control[`J_TYPE]) begin
+            case(opcode)
+	            `J:
+		          //outData = insn_index;
+	              bt = 1'b1;
+		        
+	            `BEQ:
+		          if (rsData == rtData) begin
+		              outData = pc + $signed(offset << 2);
+		              bt = 1'b1;
+		          end else begin
+		              bt = 1'b0;
+		          end
+	            `BNE:
+		          if  (rsData  != rtData) begin
+		              outData = pc + $signed(offset << 2);
+		              bt = 1'b1;
+		          end else begin
+		              bt = 1'b0;
+		          end
+	            `BGTZ:
+		          if ($signed(rsData) > 1'b0) begin
+		              outData = pc + $signed(offset << 2);
+		              bt = 1'b1;
+		          end else begin
+		              bt = 1'b0;
+		          end
+	            `BLEZ:
+		          if ($signed(rsData) <= 1'b0) begin
+		              outData = pc + $signed(offset << 2);
+		          end else begin
+		              bt = 1'b0;
+		          end
 
-	      `REGIMM:
-		case(rt)
-		  `BLTZ:
-		    if ($signed(rsData) < 1'b0) begin
-			outData = pc + $signed(offset << 2);
-			bt = 1'b1;
-		    end else begin
-			bt = 1'b0;
-		    end
-		  `BGEZ:
-		    if ($signed(rsData) >= 1'b0) begin
-			outData = pc + $signed(offset << 2);
-			bt = 1'b1;
-		    end else begin
-			bt = 1'b0;
-		    end
-		endcase // case (rtData)
-
-	    endcase // case (funct)
-
-	end
-
+	            `REGIMM:
+		          case(rt)
+		              `BLTZ:
+		                if ($signed(rsData) < 1'b0) begin
+			                outData = pc + $signed(offset << 2);
+			                bt = 1'b1;
+		                end else begin
+			                bt = 1'b0;
+		                end
+		              `BGEZ:
+		                if ($signed(rsData) >= 1'b0) begin
+			                outData = pc + $signed(offset << 2);
+			                bt = 1'b1;
+		                end else begin
+			                bt = 1'b0;
+		                end
+		          endcase // case (rtData)
+	        endcase // case (funct)
+	    end
     end
     
     always @(posedge clock)
