39128c35b6adede20d2448129f9f2c6bc90d971c
Stephan van den Heuvel <stephanvandenheuvel@gmail.com>
2012-07-24 16:12:34 -0400

    added nicer looking logging to decode.

--- pipeline.v
+++ pipeline.v
@@ -84,7 +84,7 @@ module pipeline(
     reg         pipeline_stall;
     reg         instruction_valid; 
     // SREC Parser
-    srec_parser #("srec_files/SimpleIf.srec") srec0(
+    srec_parser #("srec_files/BubbleSort.srec") srec0(
         .clock          (clock),
         .mem_address    (srec_address),
         .mem_wren       (srec_wren),
@@ -221,10 +221,12 @@ module pipeline(
 
     always @(posedge clock) begin
         if (alu_insn_out[26:31] == `JR && alu_out_data== 31) begin
-           program_done <= 1;
-           decode_dump_regs <= 1;
-	       mem_stage_print_stack <= 1;
+            program_done <= 1;
+            decode_dump_regs <= 1;
+	    mem_stage_print_stack <= 1;
 	    @(posedge clock);
+	    mem_stage_print_stack <= 0;
+	    decode_dump_regs <= 0;
         end
     end
     always @(posedge clock) begin
@@ -234,19 +236,18 @@ module pipeline(
         if (fetch_address < 32'h8002_0000) begin
             instruction_valid = 1'b0;
             pipeline_stall = 1'b1;
-	        decode_insn_valid = 1'b0;
+	    decode_insn_valid = 1'b0;
         end else begin 
             instruction_valid = 1'b1;
-	        decode_insn_valid = 1'b1;
+	    decode_insn_valid = 1'b1;
             pipeline_stall = 1'b0;
 
-	    $display("\n\n %x ", fetch_pc_out);
-	    $display("decode instruction out: %X", alu_insn_in);
+
+	    $display("\n\n PC: %x ", fetch_pc_out - 4);
 	    $display("fetched instruction: %X", fetch_data_out);
-	    $display("decode stall: %d", decode_stall_out);
-	    $display("fetch pc out: %X", fetch_pc_out);
-	    $display("ALU register WE: %d", decode_control[`REG_WE]);
-	    $display("ALU RD: %d\n\n", decode_rd_out);
+	    $display("decode instruction: %X", alu_insn_in);
+	    $display("executing instruction: %X", alu_insn_out);
+
 
         end
     end
